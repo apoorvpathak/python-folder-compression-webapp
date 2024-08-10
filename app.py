348426467816f95d4f63ef2016bbe359785e30ed
Apoorv Pathak <apoorvworks@gmail.com>
2024-08-10 20:35:58 +0530

    Update app.py with Port Config

--- app.py
+++ app.py
@@ -42,6 +42,4 @@ def upload(): #UPLOAD LOGIC
     return send_file(zip_filepath, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
